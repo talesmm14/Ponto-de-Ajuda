3bcefc7d1c6325fec4a947563ce1d404d76e2ba3
Tales <talesmelquiades@hotmail.com>
2020-08-07 18:53:03 -0300

    Models

---
+++ PontoDeAjuda/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PontoDeAjuda.Models
+{
+    public class Usuario
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int UsuarioID { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Senha { get; set; }
+    }
+}
