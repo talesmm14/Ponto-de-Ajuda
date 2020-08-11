b747f1e2df641d42d6d767ee0005002066e8804b
Tales <talesmelquiades@hotmail.com>
2020-08-11 20:31:18 -0300

    Adicionando classes principais, (Ponto de ajudas e suprimentos)

---
+++ src/PontodeAjuda.Core/Models/Pontos/Ponto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+
+namespace PontodeAjuda
+{
+    [Table("Pontos")]
+    public class Ponto : Entity, IHasCreationTime
+    {
+        public const int MaxTitleLength = 256;
+        public const int MaxDescriptionLength = 64 * 1024;
+
+        [Required]
+        [StringLength(MaxTitleLength)]
+        public string Nome { get; set; }
+        [StringLength(MaxDescriptionLength)]
+        public string Descricao { get; set; }
+        [Required]
+        [StringLength(MaxDescriptionLength)]
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataFinal { get; set; }
+        [Required]
+        public Suprimento Suprimentos { get; set; }
+        public DateTime CreationTime { get; set; }
+        public Ponto()
+        {
+            CreationTime = Clock.Now;
+            DataFinal = DataFinal;
+        }
+
+        public Ponto(string nome, string descricao, string endereco, string telefone, DateTime dataFinal, Suprimento suprimentos)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Endereco = endereco;
+            Telefone = telefone;
+            DataFinal = dataFinal;
+            Suprimentos = suprimentos;
+            CreationTime = Clock.Now;
+        }
+    }
+}
---
+++ src/PontodeAjuda.Core/Models/Pontos/Suprimento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+
+namespace PontodeAjuda
+{
+    [Table("Suprimentos")]
+    public class Suprimento : Entity
+    {
+            [Required]
+            public string Nome { get; set; }
+            [Required]
+            public string icon { get; set; }
+        }
+}
+
