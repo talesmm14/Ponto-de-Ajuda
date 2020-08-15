85d87ea3833f111b45420e26159eeaf47627a83e
Tales <talesmelquiades@hotmail.com>
2020-08-15 04:13:53 -0300

    Parte rafael

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,13 @@ namespace PontoDeAjuda.Controllers
             return View();
         }
 
+        public ActionResult Covid()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
